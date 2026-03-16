b00a6e56cbd1fa6dbd8e7b0936dc5e618292cd27
Fredy Zarate <ing.fredyzarate@outlook.com>
2026-03-15 20:34:53 -0500

    📚 Return pattern added

---
+++ Application/Common/Result.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+
+namespace Vivigest_backend.Application.Common
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T? Value { get; set; }
+        public Error Error { get; set; }
+    
+
+        public Result(bool isSuccess, Error error, T? value = default)
+        {
+            IsSuccess = isSuccess;
+            Error = error;
+            Value = value;
+        }
+
+        public static Result<T> Success(T value)
+            => new Result<T>(true, Error.None, value);
+
+        public static Result<T> Failure(Error error)
+            => new Result<T>(false, error);
+    }
+}
+
