f9eb7486c151d66e31996340f78546088d3bf72a
Fredy Zarate <ing.fredyzarate@outlook.com>
2026-03-19 12:44:10 -0500

    Create 20260319173810_Vivigest_Database.cs

---
+++ Migrations/20260319173810_Vivigest_Database.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vivigest_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Vivigest_Database : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
