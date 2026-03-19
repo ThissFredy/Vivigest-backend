410d6b42cdee55b934e90bf4962abd0025763540
Fredy Zarate <ing.fredyzarate@outlook.com>
2026-03-18 19:08:24 -0500

    🔩 Dependences injection

--- Program.cs
+++ Program.cs
@@ -1,27 +1,41 @@
 using Microsoft.EntityFrameworkCore;
-using Scalar.AspNetCore;
 using Vivigest_backend.Application.Interfaces.IAuth;
+using Vivigest_backend.Application.Interfaces.IRepository;
+using Vivigest_backend.Application.Interfaces.IService;
+using Vivigest_backend.Application.Services;
 using Vivigest_backend.Infrastructure.Authentication;
 using Vivigest_backend.Infrastructure.Persistance;
+using Vivigest_backend.Infrastructure.Repositories;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Vivigest_backend.Application.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<VivigestDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Vivigestconnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IJwtProvider, JwtProvider>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IDocumentTypeService, DocumentTypeService>();
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<RegisterUserValidator>();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
